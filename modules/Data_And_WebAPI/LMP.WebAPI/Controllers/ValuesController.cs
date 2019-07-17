dce3a0e32bc36fc3ef3d29ea8a4d1b188bc0f20a
Sergei Petrashko <sergei.petrashko8@gmail.com>
2019-07-17 11:44:05 +0300

    Revert "Added empty controllers."
    This reverts commit 90392700cb821d29522b1c5e0702426449c0a1a4.

--- modules/Data_And_WebAPI/LMP.WebAPI/Controllers/ValuesController.cs
+++ modules/Data_And_WebAPI/LMP.WebAPI/Controllers/ValuesController.cs
@@ -1,43 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMP.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class QuestionsManagementController : ControllerBase
+    public class ValuesController : ControllerBase
     {
-        // GET: api/QuestionsManagement
+        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/QuestionsManagement/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST: api/QuestionsManagement
+        // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/QuestionsManagement/5
+        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
