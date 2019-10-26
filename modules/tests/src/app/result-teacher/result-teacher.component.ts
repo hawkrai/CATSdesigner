ae958ac4e18d64d61d83ee6c17a9b401807046d8
SarLayer <jekapolhovski@mail.ru>
2019-10-26 14:48:44 +0300

    question description editor part 2

---
+++ modules/tests/src/app/result-teacher/result-teacher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-teacher',
+  templateUrl: './result-teacher.component.html',
+  styleUrls: ['./result-teacher.component.css']
+})
+export class ResultTeacherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
