2c139aa3c56f3af1a4dcff16942eda6792c6cd9c
hellmar <helmar.sayko@bk.ru>
2020-05-17 21:13:09 +0300

    fix:

--- modules/subjects/src/app/app.component.ts
+++ modules/subjects/src/app/app.component.ts
@@ -17,8 +17,8 @@ export class AppComponent implements OnInit{
               private store: Store<IAppState>) { }
 
   ngOnInit(): void {
-    localStorage.setItem('currentSubject', JSON.stringify({id: "2026", Name:"Тестирование ПО"}));
-    localStorage.setItem('currentUser', JSON.stringify({id: 2, role: 'lector', userName: 'popova'}));
+    // localStorage.setItem('currentSubject', JSON.stringify({id: "2026", Name:"Тестирование ПО"}));
+    // localStorage.setItem('currentUser', JSON.stringify({id: 2, role: 'lector', userName: 'popova'}));
     const group: {id: '', Name: ''} = JSON.parse(localStorage.getItem('currentSubject'));
     const user: User = JSON.parse(localStorage.getItem('currentUser'));
     group && this.store.dispatch(new SetSubject(group));
