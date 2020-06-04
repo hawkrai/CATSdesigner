645769e5410ab7b3a0b338e4878fc79792a016b3
hellmar <helmar.sayko@bk.ru>
2020-06-04 22:58:56 +0300

    fix: files module view

--- modules/subjects/src/app/modules/files/files.component.ts
+++ modules/subjects/src/app/modules/files/files.component.ts
@@ -11,16 +11,40 @@ import {getSubjectId} from '../../store/selectors/subject.selector';
 })
 export class FilesComponent implements OnInit {
 
+  public files = [];
 
   constructor(private fileService: FileService,
               private store$: Store<IAppState>) { }
 
   ngOnInit() {
     this.store$.pipe(select(getSubjectId)).subscribe(subjectId => {
-      this.fileService.getSubjectFile({subjectId}).subscribe(files => {
-        console.log(files)
+      this.fileService.getSubjectFile({subjectId}).subscribe(attachments => {
+        console.log(attachments)
+        this.attachmentsToFiles([...attachments.Lectures, ...attachments.Labs, ...attachments.Practicals])
       })
     });
   }
 
+  attachmentsToFiles(attachments) {
+    let values = '["';
+    attachments.forEach((attachment, index) => {
+      values += attachment.Name + '/' + attachment.Id + '/' + attachment.PathName + '/' +
+        attachment.FileName;
+      if (index < attachments.length - 1) {
+        values += '","'
+      }
+    });
+
+    values += '"]';
+
+    if (attachments.length) {
+      this.fileService.getAttachment({values, deleteValues: 'DELETE'})
+        .subscribe(files => this.files = files);
+    }
+  }
+
+  deleteFile(file) {
+    this.fileService.deleteFile(file.DeleteUrl)
+      .subscribe(res => console.log(res));
+  }
 }
--- modules/subjects/src/app/modules/files/files.module.ts
+++ modules/subjects/src/app/modules/files/files.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilesComponent } from './files.component';
+import {MatModule} from '../../mat.module';
 
 
 
 @NgModule({
   declarations: [FilesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatModule
   ]
 })
 export class FilesModule { }
