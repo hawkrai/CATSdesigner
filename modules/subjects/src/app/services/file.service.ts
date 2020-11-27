30b2950f45480cfa6d4f7652a5605b6a9c9aab9d
leha.vanyukevich <leha.vanyukevich@gmail.com>
2020-11-28 00:17:57 +0300

    Display subject files

--- modules/subjects/src/app/services/file.service.ts
+++ modules/subjects/src/app/services/file.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,9 @@ export class FileService {
   }
 
   public getSubjectFile(params: any): Observable<any> {
-    return this.http.get('Subject/GetFileSubjectV2', {params: new HttpParams({ fromObject: params })});
+    return this.http.get<any>('Subject/GetFileSubjectV2', {params: new HttpParams({ fromObject: params })}).pipe(
+      map(response => response.Attachment)
+    );
   }
   
   public deleteFile(deleteUrl: string): Observable<any> {
