34810a8f30924a2c91a20540c142173f4a27255d
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-04-10 10:07:53 +0300

    add build step

--- build/build_modules.sh
+++ build/build_modules.sh
@@ -3,6 +3,7 @@ export admin_path=$modules_path'admin'
 export tests_path=$modules_path'/tests'
 export subjects_path=$modules_path'/subjects'
 export cp_path=$modules_path'/course-projects'
+export dp_path=$modules_path'/diplom-projects'
 export confirmation_path=$modules_path'/confirmation'
 export editor_path=$modules_path'/editor'
 export complex_path=$modules_path'/complex'
@@ -21,6 +22,10 @@ npm i
 npm run build
 
 cd $cp_path
+npm i
+npm run build
+
+cd $dp_path
 npm i
 npm run build
 
