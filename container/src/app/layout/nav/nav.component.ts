e56ceb9ff168773f8b8762a0141a5f285c7cdcfc
SarLayer <jekapolhovski@mail.ru>
2021-05-31 08:56:52 +0300

    refactoring

--- container/src/app/layout/nav/nav.component.ts
+++ container/src/app/layout/nav/nav.component.ts
@@ -4,15 +4,15 @@ import {AuthenticationService} from "../../core/services/auth.service";
 import {first, takeUntil, tap} from "rxjs/operators";
 import {CoreService} from "../../core/services/core.service";
 import {Subject} from "rxjs";
-import { Lecturer, Student, Group } from '../../core/models/searchResults/search-results';
-import { SearchService } from '../../core/services/searchResults/search.service';
-import { ProfileService } from '../../core/services/searchResults/profile.service';
-import { MenuService } from "src/app/core/services/menu.service";
-import { MatDialog } from "@angular/material/dialog";
-import {AboutSystemPopoverComponent} from '../../about-system/about-popover/about-popover.component';
+import {Group, Lecturer, Student} from "../../core/models/searchResults/search-results";
+import {SearchService} from "../../core/services/searchResults/search.service";
+import {ProfileService} from "../../core/services/searchResults/profile.service";
+import {MenuService} from "src/app/core/services/menu.service";
+import {MatDialog} from "@angular/material/dialog";
+import {AboutSystemPopoverComponent} from "../../about-system/about-popover/about-popover.component";
 
 
-interface Locale {
+interface DropDownValue {
   name: string;
   value: string
 }
@@ -27,20 +27,18 @@ export class NavComponent implements OnInit, OnDestroy {
   public isLector: boolean;
   public isAdmin: boolean;
   public unconfirmedStudents: number = 0;
-  public locales: Locale[] = [{name: "Ru", value: "ru"}, {name: "En", value: "en"}];
-  public locale: Locale;
-  private unsubscribeStream$: Subject<void> = new Subject<void>();
-  public profileIcon = "/assets/images/account.png";;
-
+  public locales: DropDownValue[] = [{name: "Ru", value: "ru"}, {name: "En", value: "en"}];
+  public locale: DropDownValue;
+  public profileIcon = "/assets/images/account.png";
   public currentUserId!: number;
+  public themes: DropDownValue[] = [{name: "White", value: "white"}, {name: "Dark", value: "dark"}];
+  public theme: DropDownValue;
   valueForSearch!: string;
-  
   searchResults !: string[];
-
   lecturerSearchResults!: Lecturer[];
   studentSearchResults!: Student[];
   groupSearchResults!: Group[];
-
+  private unsubscribeStream$: Subject<void> = new Subject<void>();
 
   constructor(private layoutService: LayoutService,
               private coreService: CoreService,
@@ -49,20 +47,21 @@ export class NavComponent implements OnInit, OnDestroy {
               private profileService: ProfileService,
               private menuService: MenuService,
               public dialog: MatDialog
-              ) {
+  ) {
   }
 
   get logoWidth(): string {
     const width = this.menuService.getSideNavWidth();
-    return width ? `${width - 16}px` : 'auto';
+    return width ? `${width - 16}px` : "auto";
   }
 
   public ngOnInit(): void {
     this.isLector = this.autService.currentUserValue.role == "lector";
     this.isAdmin = this.autService.currentUserValue.role == "admin";
+    localStorage.setItem("theme", "white");
     this.getAvatar();
     const local: string = localStorage.getItem("locale");
-    this.locale = local ? this.locales.find((locale: Locale) => locale.value === local) : this.locales[0];
+    this.locale = local ? this.locales.find((locale: DropDownValue) => locale.value === local) : this.locales[0];
 
 
     this.currentUserId = this.autService.currentUserValue.id;
@@ -91,6 +90,11 @@ export class NavComponent implements OnInit, OnDestroy {
 
   public onValueChange(value: any): void {
     localStorage.setItem("locale", value.value.value);
+    window.location.reload();
+  }
+
+  public themeChange(value: any): void {
+    localStorage.setItem("theme", value.value.value);
     window.location.reload()
   }
 
@@ -98,7 +102,6 @@ export class NavComponent implements OnInit, OnDestroy {
     this.unsubscribeStream$.next(null);
     this.unsubscribeStream$.complete();
   }
-
 
 
   getAvatar() {
@@ -127,7 +130,7 @@ export class NavComponent implements OnInit, OnDestroy {
 
   viewLecturerSearchResults() {
     this.searchService.getLecturerSearchResults(this.valueForSearch).subscribe(res => {
-       this.lecturerSearchResults = res;
+      this.lecturerSearchResults = res;
     });
   }
 
@@ -146,17 +149,17 @@ export class NavComponent implements OnInit, OnDestroy {
   public routeToAboutPopover() {
 
     const dialogRef = this.dialog.open(AboutSystemPopoverComponent, {
-      width: '600px',
-      height: '60%', 
-      position: {top: '128px'}
-    }); 
+      width: "600px",
+      height: "60%",
+      position: {top: "128px"}
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result != null) {
       }
-})
+    });
 
-    
-}
+
+  }
 
 }
