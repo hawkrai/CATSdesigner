6bb3bbdaab2ccb9bfbf83682ee6b490478a3b53a
zxzSmilezxz <bahya2001qw@gmail.com>
2021-10-10 16:15:02 +0300

    build fixes

--- build/build_modules.sh
+++ build/build_modules.sh
@@ -10,42 +10,62 @@ export complex_path=$modules_path'/complex'
 export schedule_path=$modules_path'/schedule'
 export statistics_path=$modules_path'/statistics'
 
-cd $admin_path
+cd $admin_pathz
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $tests_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $subjects_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $cp_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $dp_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $confirmation_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $complex_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $editor_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $schedule_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
 
 cd $statistics_path
+npx rimraf ./node_modules
+npm rimraf ./package-lock.json
 npm i
 npm run build:qa
