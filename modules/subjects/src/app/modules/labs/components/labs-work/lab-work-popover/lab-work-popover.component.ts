420295e36c4e2a2ee6d6f13707df9369f6630d8d
alexey.vanyukevich <alexey.vanyukevich@gmail.com>
2021-11-09 00:51:07 +0300

    Add lab validators

--- modules/subjects/src/app/modules/labs/components/labs-work/lab-work-popover/lab-work-popover.component.ts
+++ modules/subjects/src/app/modules/labs/components/labs-work/lab-work-popover/lab-work-popover.component.ts
@@ -8,6 +8,7 @@ import { IAppState } from 'src/app/store/state/app.state';
 import { FilesService } from 'src/app/services/files.service';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { attchedFileConverter } from 'src/app/utils';
+import { whitespace } from 'src/app/shared/validators/whitespace.validator';
 
 
 @Component({
@@ -35,8 +36,8 @@ export class LabWorkPopoverComponent extends BaseFileManagementComponent impleme
     this.loadAttachments();
     this.labForm = new FormGroup({
       id: new FormControl(this.data.model.id),
-      theme: new FormControl(this.data.model.theme, [Validators.required, Validators.maxLength(256)]),
-      duration: new FormControl(this.data.model.duration, [Validators.required, Validators.min(1), Validators.max(5)]),
+      theme: new FormControl(this.data.model.theme, [Validators.required, whitespace, Validators.maxLength(256)]),
+      duration: new FormControl(this.data.model.duration, [Validators.required, Validators.min(1), Validators.max(36)]),
       order: new FormControl(this.data.model.order),
       pathFile: new FormControl(this.data.model.pathFile),
       shortName: new FormControl(this.data.model.shortName),
