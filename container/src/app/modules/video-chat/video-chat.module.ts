ef4fb203cef756e51e9abc72430e4633544420ac
Siarhei Kharlap <sergeugoodgood@gmail.com>
2021-11-16 20:05:59 +0300

    Add dragable component

--- container/src/app/modules/video-chat/video-chat.module.ts
+++ container/src/app/modules/video-chat/video-chat.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatModule } from './../chat/chat.module';
 import { VideoHandlerComponent } from './components/video-handler/video-handler.component';
-
+import { AngularDraggableModule } from 'ngx-draggable-resize';
 
 
 @NgModule({
   declarations: [VideoHandlerComponent],
   imports: [
   CommonModule,
-  ChatModule
+  ChatModule,
+  AngularDraggableModule
   ],
   exports: [
     VideoHandlerComponent,
