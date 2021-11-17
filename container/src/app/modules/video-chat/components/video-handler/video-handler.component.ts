ed9ea8192a29292d92d1fc9fbbacbdfd495c6298
Siarhei Kharlap <sergeugoodgood@gmail.com>
2021-11-17 12:13:47 +0300

    Add resizing to component

--- container/src/app/modules/video-chat/components/video-handler/video-handler.component.ts
+++ container/src/app/modules/video-chat/components/video-handler/video-handler.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SignalRService } from './../../../chat/shared/services/signalRSerivce';
 
 @Component({
   selector: 'app-video-handler',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoHandlerComponent implements OnInit {
 
-  constructor() { }
+  constructor(signalService:SignalRService) { }
 
   ngOnInit(): void {
   }
