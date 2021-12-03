c9c40197efe26e4bbce4056a1596cf6ae5df1159
alexey.vanyukevich <alexey.vanyukevich@gmail.com>
2021-12-04 02:23:53 +0300

    Protect services

--- api/Application.Core/Helpers/UserContext.cs
+++ api/Application.Core/Helpers/UserContext.cs
@@ -14,14 +14,9 @@ namespace Application.Core.Helpers
 
         public static string Id { get; set; }
 
-        private static string _role;
-        public static string Role
-        {
-            get => GetRole();
-            set => _role = value;
-        }
+        public static string Role { get; set; }
 
-        public static int CurrentUserId => UserId();
+        public static int CurrentUserId => WebSecurity.CurrentUserId <= 0 ? int.Parse(Id) : WebSecurity.CurrentUserId;
 
         public static string CurrentUserName =>
             string.IsNullOrEmpty(WebSecurity.CurrentUserName) ? Name : WebSecurity.CurrentUserName;
@@ -53,31 +48,5 @@ namespace Application.Core.Helpers
             return WebSecurity.CurrentUserId <= 0 ? int.Parse(Id) : WebSecurity.CurrentUserId; ;
         }
 
-        static string GetRole()
-        {
-            var authCookie = HttpContext.Current.Request.Cookies["Authorization"];
-
-            var autHeader = HttpContext.Current.Request.Headers["Authorization"];
-            if (authCookie != null || autHeader != null)
-            {
-                var token = authCookie != null ? authCookie.Value : autHeader.Replace("Bearer", "");
-                try
-                {
-                    var tokenSecret = ConfigurationManager.AppSettings["jwt:secret"];
-                    var json = new JwtBuilder()
-                        .WithSecret(tokenSecret)
-                        .WithAlgorithm(new HMACSHA256Algorithm())
-                        .MustVerifySignature()
-                        .Decode<IDictionary<string, string>>(token);
-                    var role = json[ClaimsIdentity.DefaultRoleClaimType];
-                    return !string.IsNullOrWhiteSpace(role) ? role : _role;
-                }
-                catch
-                {
-                    return _role;
-                }
-            }
-            return _role;
-        }
     }
 }
