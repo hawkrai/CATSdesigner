98cc4ea9d9c08af8177042f49355a050468345d9
alexey.vanyukevich <alexey.vanyukevich@gmail.com>
2021-12-28 00:02:55 +0300

    Added course to group statistics

--- api/LMPlatform.UI/Services/Parental/ParentalService.svc.cs
+++ api/LMPlatform.UI/Services/Parental/ParentalService.svc.cs
@@ -63,7 +63,9 @@ namespace LMPlatform.UI.Services.Parental
                         .Include(e => e.Students.Select(x => x.ScheduleProtectionLabMarks.Select(t => t.ScheduleProtectionLab).Select(f => f.SubGroup).Select(s => s.SubjectGroup)))
                         .Include(e => e.Students.Select(x => x.StudentLabMarks.Select(t => t.Lab)))
                         .Include(e => e.Students.Select(x => x.ScheduleProtectionPracticalMarks.Select(t => t.ScheduleProtectionPractical).Select(s => s.Group).Select(s => s.SubjectGroups)))
-                        .Include(e => e.Students.Select(x => x.StudentPracticalMarks.Select(x => x.Practical))));
+                        .Include(e => e.Students.Select(x => x.StudentPracticalMarks.Select(x => x.Practical)))
+                        .Include(e => e.Students.Select(x => x.CoursePercentagesResults.Select(x => x.CoursePercentagesGraph)))
+                        .Include(e => e.Students.Select(x => x.AssignedCourseProjects.Select(x => x.CourseProject))));
                     var subjects = SubjectManagementService.GetGroupSubjects(id, archive);
                     var students = group.Students.ToList();
                     students.Sort((arg1, arg2) => { return string.Compare(arg1.FullName, arg2.FullName); });
