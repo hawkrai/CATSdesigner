97513b2c747dea25d118e9e75ff02c599e5a9d74
leha.vanyukevich <leha.vanyukevich@gmail.com>
2022-05-16 23:26:52 +0300

    Automaticaly update job protection after student or teacher action

---
+++ modules/subjects/src/app/models/protection-type.enum.ts
@@ -0,0 +1,4 @@
+export enum ProtectionType {
+    Lab,
+    Practical
+}
---
+++ modules/subjects/src/app/models/protection.model.ts
@@ -0,0 +1,9 @@
+import { ProtectionType } from "./protection-type.enum";
+
+export class Protection {
+    subjectId: number;
+    userId: number;
+    groupId: number;
+    protectionType: ProtectionType;
+    from: number;
+}
