69dec52fd9432661ce66463839dd87684d2f3459
MonticABC <nikitaperegud@gmail.com>
2022-05-18 23:34:40 +0300

    add archive subjects for Teacher

--- modules/statistics/src/app/main-page/main-page.component.ts
+++ modules/statistics/src/app/main-page/main-page.component.ts
@@ -33,31 +33,38 @@ export class MainPageComponent implements OnInit {
   labMarks: any[] = [];
   testMarks: any[] = [];
   conMarks: any[] = [];
+  ratingMarksArchive: any[] = [];
+  practMarksArchive: any[] = [];
+  labMarksArchive: any[] = [];
+  testMarksArchive: any[] = [];
+  conMarksArchive: any[] = [];
   ratingMarks: any[] = [];
   subjectName: any[] = [];
+  subjectNameArchive: any[] = [];
   categories: string[][] = [[]];
   categoriesTemp: string[] = [];
   series: any[] = [];
+  seriesArchive: any[] = [];
   colorsTemp: any[] = [];
   checked: any[] = [false, false, false, false];
   isArchived = false;
+  isArchiveTeacher = false;
 
 
+  colors: string [] = ['orange', 'red', 'blue', 'purple', 'green'];
 
-  colors: string [] = [ 'orange', 'red', 'blue', 'purple' , 'green'];
 
-
-  public categoriesConst: string[]  = [this.translatePipe.transform('text.statistics.avg.pract', 'Средний балл за практические занятия'),
-    this.translatePipe.transform('text.statistics.avg.lab', 'Средний балл за лабораторные работы') ,
+  public categoriesConst: string[] = [this.translatePipe.transform('text.statistics.avg.pract', 'Средний балл за практические занятия'),
+    this.translatePipe.transform('text.statistics.avg.lab', 'Средний балл за лабораторные работы'),
     this.translatePipe.transform('text.statistics.avg.test', 'Средняя оценка за тесты'),
     this.translatePipe.transform('text.statistics.avg.course', 'Оценка за курсовой проект'),
     this.translatePipe.transform('text.statistics.avg.rating', 'Рейтинговая оценка')];
 
-  constructor(private serviceService: StatisitcsServiceService, private translatePipe: TranslatePipe ) {
+  constructor(private serviceService: StatisitcsServiceService, private translatePipe: TranslatePipe) {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
     if (this.user.role == 'student') {
       this.serviceService.getUserInfo(this.user.id).subscribe(res => {
-        this.serviceService.getLabsStastics(res.GroupId).subscribe(result => {
+        this.serviceService.getUserStastics().subscribe(result => {
           if (result.Message == 'Ok') {
             this.serviceService.getAllSubjects(this.user.userName).subscribe(subjects => {
               this.performData(subjects, result, this.listChartOptions, false);
@@ -73,90 +80,139 @@ export class MainPageComponent implements OnInit {
     } else {
       this.serviceService.getTeacherStatistics().subscribe(res => {
         res.SubjectStatistics.forEach(subject => {
-          this.serviceService.getCheckedType(subject.SubjectId).subscribe(types => {
-            this.checked = [false, false, false, false];
-            types.forEach(type => {
-              if (type.ModuleId == 13) {
-                this.checked[0] = true;
+          this.serviceService.getGroupsBySubjectId(subject.SubjectId).subscribe(groups => {
+            this.serviceService.getCheckedType(subject.SubjectId).subscribe(types => {
+              this.isArchiveTeacher = false;
+              if(groups.Groups.length == 0) {
+                this.isArchiveTeacher = true;
               }
-              if (type.ModuleId == 3) {
-                this.checked[1] = true;
+              this.checked = [false, false, false, false];
+              types.forEach(type => {
+                if (type.ModuleId == 13) {
+                  this.checked[0] = true;
+                }
+                if (type.ModuleId == 3) {
+                  this.checked[1] = true;
+                }
+                if (type.ModuleId == 8) {
+                  this.checked[2] = true;
+                }
+                if (type.ModuleId == 4) {
+                  this.checked[3] = true;
+                }
+              });
+              this.temp = [];
+              this.categoriesTemp = [];
+              this.colorsTemp = [];
+              this.tempAvg = 0;
+              if (this.checked[0]) {
+                if (this.isArchiveTeacher) {
+                  this.practMarksArchive.push(this.serviceService.round(+subject.AveragePracticalsMark));
+                } else {
+                  this.practMarks.push(this.serviceService.round(+subject.AveragePracticalsMark));
+                }
+                this.colorsTemp.push(this.colors[0]);
+                this.temp.push(this.serviceService.round(+subject.AveragePracticalsMark));
+                this.categoriesTemp.push(this.categoriesConst[0]);
+              } else {
+                if (this.isArchiveTeacher) {
+                  this.practMarksArchive.push(0);
+                } else {
+                  this.practMarks.push(0);
+                }
               }
-              if (type.ModuleId == 8) {
-                this.checked[2] = true;
+
+              if (this.checked[1]) {
+                if (this.isArchiveTeacher) {
+                  this.labMarksArchive.push(this.serviceService.round(+subject.AverageLabsMark));
+                } else {
+                  this.labMarks.push(this.serviceService.round(+subject.AverageLabsMark));
+                }
+                this.colorsTemp.push(this.colors[1]);
+                this.categoriesTemp.push(this.categoriesConst[1]);
+                this.temp.push(this.serviceService.round(+subject.AverageLabsMark));
+              } else {
+                if (this.isArchiveTeacher) {
+                  this.labMarksArchive.push(0);
+                } else {
+                  this.labMarks.push(0);
+                }
               }
-              if (type.ModuleId == 4) {
-                this.checked[3] = true;
+
+              if (this.checked[2]) {
+                if (this.isArchiveTeacher) {
+                  this.testMarksArchive.push(this.serviceService.round(+subject.AverageTestsMark));
+                } else {
+                  this.testMarks.push(this.serviceService.round(+subject.AverageTestsMark));
+                }
+                this.colorsTemp.push(this.colors[2]);
+                this.temp.push(this.serviceService.round(+subject.AverageTestsMark));
+                this.categoriesTemp.push(this.categoriesConst[2]);
+              } else {
+                if (this.isArchiveTeacher) {
+                  this.testMarksArchive.push(0);
+                } else {
+                  this.testMarks.push(0);
+                }
+              }
+
+              if (this.checked[3]) {
+                if (this.isArchiveTeacher) {
+                  this.conMarksArchive.push(this.serviceService.round(+subject.AverageCourceProjectMark));
+                } else {
+                  this.conMarks.push(this.serviceService.round(+subject.AverageCourceProjectMark));
+                }
+                this.colorsTemp.push(this.colors[3]);
+                this.temp.push(this.serviceService.round(+subject.AverageCourceProjectMark));
+                this.categoriesTemp.push(this.categoriesConst[3]);
+              } else {
+                if (this.isArchiveTeacher) {
+                  this.conMarksArchive.push(0);
+                } else {
+                  this.conMarks.push(0);
+                }
+              }
+              if (this.categoriesTemp.length != 0) {
+                this.temp.forEach(el => {
+                  this.tempAvg += el;
+                });
+                const rating = this.serviceService.round(this.tempAvg / this.temp.length);
+                this.ratingMarks.push(rating);
+                this.temp.push(rating);
+                this.categoriesTemp.push(this.categoriesConst[4]);
+                if (this.isArchiveTeacher) {
+                  this.addChart(this.temp, subject.SubjectName, this.categoriesTemp, subject.SubjectId,
+                    this.listChartOptionsArchive, this.colorsTemp);
+                  this.subjectNameArchive.push(subject.SubjectName);
+                } else {
+                  this.addChart(this.temp, subject.SubjectName, this.categoriesTemp,
+                    subject.SubjectId, this.listChartOptions, this.colorsTemp);
+                  this.subjectName.push(subject.SubjectName);
+                }
+              }
+              if (res.SubjectStatistics.indexOf(subject) == res.SubjectStatistics.length - 1) {
+                this.series.push({name: this.categoriesConst[0], data: this.practMarks});
+                this.series.push({name: this.categoriesConst[1], data: this.labMarks});
+                this.series.push({name: this.categoriesConst[2], data: this.testMarks});
+                this.series.push({name: this.categoriesConst[3], data: this.conMarks});
+                this.series.push({name: this.categoriesConst[4], data: this.ratingMarks});
+                this.seriesArchive.push({name: this.categoriesConst[0], data: this.practMarksArchive});
+                this.seriesArchive.push({name: this.categoriesConst[1], data: this.labMarksArchive});
+                this.seriesArchive.push({name: this.categoriesConst[2], data: this.testMarksArchive});
+                this.seriesArchive.push({name: this.categoriesConst[3], data: this.conMarksArchive});
+                this.seriesArchive.push({name: this.categoriesConst[4], data: this.ratingMarksArchive});
+                // this.series.push({name: this.categoriesConst[0], data: [7.4, 5.5, 0,   0,   6.7 ]});
+                // this.series.push({name: this.categoriesConst[1], data: [0,   0,   6.3, 7.7, 6.5 ]});
+                // this.series.push({name: this.categoriesConst[2], data: [6.7, 8.2, 6.2, 5.9, 0  ]});
+                // this.series.push({name: this.categoriesConst[3], data: [0,   0,   0,   0,   6 ]});
+                // this.series.push({name: this.categoriesConst[4], data: [7.1, 6.9, 6.3, 6.8, 6.4 ]});
+                // this.addMarksChart(this.series, ['Базы данных', 'Модульное тестирование', 'Методы и алгоритмы принятия решений', 'Основы защиты информации', 'Английский язык в профдеятельности']);
+                this.addMarksChart(this.series, this.subjectName);
+                this.addArchiveMarksChart(this.seriesArchive, this.subjectNameArchive);
               }
             });
-            this.temp = [];
-            this.categoriesTemp = [];
-            this.colorsTemp = [];
-            this.tempAvg = 0;
-            if (this.checked[0]) {
-              this.colorsTemp.push(this.colors[0]);
-              this.temp.push(this.serviceService.round(+subject.AveragePracticalsMark));
-              this.practMarks.push(this.serviceService.round(+subject.AveragePracticalsMark));
-              this.categoriesTemp.push(this.categoriesConst[0]);
-            } else {
-              this.practMarks.push(0);
-            }
-
-            if (this.checked[1]) {
-              this.colorsTemp.push(this.colors[1]);
-              this.categoriesTemp.push(this.categoriesConst[1]);
-              this.temp.push(this.serviceService.round(+subject.AverageLabsMark));
-              this.labMarks.push(this.serviceService.round(+subject.AverageLabsMark));
-            } else {
-              this.labMarks.push(0);
-            }
-
-            if (this.checked[2]) {
-              this.colorsTemp.push(this.colors[2]);
-              this.temp.push(this.serviceService.round(+subject.AverageTestsMark));
-              this.categoriesTemp.push(this.categoriesConst[2]);
-              this.testMarks.push(this.serviceService.round(+subject.AverageTestsMark));
-            } else {
-              this.testMarks.push(0);
-            }
-
-            if (this.checked[3]) {
-              this.colorsTemp.push(this.colors[3]);
-              this.temp.push(this.serviceService.round(+subject.AverageCourceProjectMark));
-              this.categoriesTemp.push(this.categoriesConst[3]);
-              this.conMarks.push(this.serviceService.round(+subject.AverageCourceProjectMark));
-            } else {
-              this.conMarks.push(0);
-            }
-            if (this.categoriesTemp.length != 0) {
-              this.temp.forEach(el => {
-                this.tempAvg += el;
-              });
-              const rating = this.serviceService.round(this.tempAvg / this.temp.length);
-              this.ratingMarks.push(rating);
-              this.temp.push(rating);
-              this.categoriesTemp.push(this.categoriesConst[4]);
-              console.log(subject.SubjectId);
-              console.log(this.temp);
-              console.log(this.categoriesTemp);
-              this.addChart(this.temp, subject.SubjectName, this.categoriesTemp, subject.SubjectId, this.listChartOptions, this.colorsTemp);
-              this.subjectName.push(subject.SubjectName);
-            }
           });
         });
-        this.series.push({name: this.categoriesConst[0], data: this.practMarks});
-        this.series.push({name: this.categoriesConst[1], data: this.labMarks});
-        this.series.push({name: this.categoriesConst[2], data: this.testMarks});
-        this.series.push({name: this.categoriesConst[3], data: this.conMarks});
-        this.series.push({name: this.categoriesConst[4], data: this.ratingMarks});
-
-        // this.series.push({name: this.categoriesConst[0], data: [7.4, 5.5, 0,   0,   6.7 ]});
-        // this.series.push({name: this.categoriesConst[1], data: [0,   0,   6.3, 7.7, 6.5 ]});
-        // this.series.push({name: this.categoriesConst[2], data: [6.7, 8.2, 6.2, 5.9, 0  ]});
-        // this.series.push({name: this.categoriesConst[3], data: [0,   0,   0,   0,   6 ]});
-        // this.series.push({name: this.categoriesConst[4], data: [7.1, 6.9, 6.3, 6.8, 6.4 ]});
-        // this.addMarksChart(this.series, ['Базы данных', 'Модульное тестирование', 'Методы и алгоритмы принятия решений', 'Основы защиты информации', 'Английский язык в профдеятельности']);
-        this.addMarksChart(this.series, this.subjectName);
       });
       // this.addChart([ 7.4, 6.7, 7.1], 'Базы данных', this.categoriesLabFree, 4112, this.listChartOptions);
       // this.addChart([5.5, 8.2, 6.9], 'Модульное тестирование', this.categoriesLabFree, 4112,  this.listChartOptions);
@@ -170,7 +226,7 @@ export class MainPageComponent implements OnInit {
   addChart(marks: any, name: string, cat: any, subjectId: any, list: any[], colorsTemp: any[]) {
     list.push(
       this.chartOptions = {
-        subject : subjectId,
+        subject: subjectId,
         colors: colorsTemp,
         categories: cat,
         series: [
@@ -255,14 +311,14 @@ export class MainPageComponent implements OnInit {
   }
 
   addMarksChart(seriesMarks: any, subjects: any) {
-    this.chartOptions1 = {
+     this.chartOptions1 = {
       series: seriesMarks,
       chart: {
         type: 'bar',
         height: Math.round(subjects.length * 35 + 90),
         stacked: true
       },
-      colors: ['#FFA500', '#ff0000', '#0000FF',    '#7F00FF' , '#006400'],
+      colors: ['#FFA500', '#ff0000', '#0000FF', '#7F00FF', '#006400'],
       plotOptions: {
         bar: {
           horizontal: true
@@ -309,7 +365,7 @@ export class MainPageComponent implements OnInit {
         height: Math.round(subjects.length * 35 + 90),
         stacked: true
       },
-      colors: ['#FFA500', '#ff0000', '#0000FF',    '#7F00FF' , '#006400'],
+      colors: ['#FFA500', '#ff0000', '#0000FF', '#7F00FF', '#006400'],
       plotOptions: {
         bar: {
           horizontal: true
@@ -405,10 +461,12 @@ export class MainPageComponent implements OnInit {
         this.labSum = 0;
         result.Students.forEach(student => {
           if (student.Id == this.user.id) {
-            this.testSum = this.serviceService.round(+student.UserAvgTestMarks.find(({Key}) => Key === subject.Id)!.Value) ;
-            this.practSum = this.serviceService.round(+student.UserAvgPracticalMarks.find(({Key}) => Key === subject.Id)!.Value);
-            this.labSum = this.serviceService.round(+student.UserAvgLabMarks.find(({Key}) => Key === subject.Id)!.Value );
-            this.courseMark = this.serviceService.round(+student.UserCoursePass.find(({Key}) => Key === subject.Id)!.Value );
+            if (student.UserAvgTestMarks.find(({Key}) => Key === subject.Id) != undefined) {
+              this.testSum = this.serviceService.round(+student.UserAvgTestMarks.find(({Key}) => Key === subject.Id)!.Value);
+              this.practSum = this.serviceService.round(+student.UserAvgPracticalMarks.find(({Key}) => Key === subject.Id)!.Value);
+              this.labSum = this.serviceService.round(+student.UserAvgLabMarks.find(({Key}) => Key === subject.Id)!.Value);
+              this.courseMark = this.serviceService.round(+student.UserCoursePass.find(({Key}) => Key === subject.Id)!.Value);
+            }
           }
         });
         if (this.checked[0]) {
@@ -450,14 +508,13 @@ export class MainPageComponent implements OnInit {
         this.temp.forEach(el => {
           this.tempAvg += el;
         });
-        const rating = this.serviceService.round(this.tempAvg / this.temp.length) ;
+        const rating = this.serviceService.round(this.tempAvg / this.temp.length);
         this.temp.push(rating);
         this.colorsTemp.push(this.colors[4]);
         this.ratingMarks.push(rating);
         this.subjectName.push(subject.Name);
         this.categoriesTemp.push(this.categoriesConst[4]);
         this.addChart(this.temp, subject.Name, this.categoriesTemp, subject.Id, chartList1, this.colorsTemp);
-        console.log(subjects.indexOf(subject) + ' ' + subjects.length);
         if (subjects.indexOf(subject) == subjects.length - 1) {
           this.series.push({name: this.categoriesConst[0], data: this.practMarks});
           this.series.push({name: this.categoriesConst[1], data: this.labMarks});
