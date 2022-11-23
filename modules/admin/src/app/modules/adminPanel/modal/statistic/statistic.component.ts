1e13ac720e25cdc8c7451c5f6abe9ce5499e95b4
GreyMarty <ikozhan9@gmail.com>
2022-11-24 00:06:04 +0300

    :sparkles: Made statistics charts avaliable offline

--- modules/admin/src/app/modules/adminPanel/modal/statistic/statistic.component.ts
+++ modules/admin/src/app/modules/adminPanel/modal/statistic/statistic.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { StatisticService } from 'src/app/service/statistic.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-statistic',
@@ -8,27 +9,61 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./statistic.component.css']
 })
 export class StatisticComponent implements OnInit {
+  
+  series = {
+    name: '',
+    data: []
+  }
 
-  title = '';
-  type = 'LineChart';
-  data = [];
-  columnNames = ['Data', 'Кол-во: '];
-  options = {
-      chartArea: {
-        width: '80%',
-        height: '70%'
-      },
-     legend: {position: 'none'},
-     hAxis: {
-        title: ''
-     },
-     vAxis: {
-        title: '',
-        viewWindow: {
-          max: 30
+  chartOptions = {
+    series: [this.series],
+    chart: {
+      width: 550,
+      height: 400,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: "",
+      align: "left"
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5
+      }
+    },
+    xaxis: {
+      tickPlacement: 'between',
+      labels: {
+        rotate: -60
+      }
+    },
+    yaxis: {
+      min: 0,
+      max: 6, 
+      labels: {
+        formatter: function(val) {
+          return val.toFixed(0);
         }
-     }
-  };
+      }
+    },
+    tooltip: {
+      y: {
+        formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+          return `Кол-во: ${value}`
+        }
+      }
+    }
+  }
   isLoad = false;
 
   constructor(private statisticService: StatisticService, public dialogRef: MatDialogRef<object>,
@@ -40,23 +75,41 @@ export class StatisticComponent implements OnInit {
 
   async loadStatistic(userId) {
     await this.statisticService.getStatistics(userId).subscribe( result => {
-      if (!result.attendance) {
-        this.data.push(['', 0]);
-      } else {
-        this.data = this.convertData(result.attendance);
+      if (result.attendance.length > 0) {
+        this.convertData(result.attendance);
       }
-      this.title = result.resultMessage;
+      else {
+        this.series.data.push({ x: "", y: 0 });
+      }
+      this.chartOptions.title.text = result.resultMessage;
       this.isLoad = true;
     });
   }
 
   convertData(array) {
-    const arr = [];
+    const MAX_COUNT = 30;
+
+    this.series.data = [];
+    
     for (const item of array) {
-      arr.push([ item.day, item.count]);
+      this.series.data.push({ x: item.day, y: item.count });
     }
-    const arrLength = arr.length;
-    return arrLength > 30 ? arr.slice( arrLength - 30, arrLength) : arr.slice(0, arrLength);
+    
+    const length = array.length;
+    
+    if (length > 30) {
+      this.series.data = this.series.data.slice(length - MAX_COUNT, length);
+    }
+
+    let maxCount = array[0].count;
+
+    for (const item of this.series.data) {
+      if (item.y > maxCount) {
+        maxCount = item.y;
+      }
+    }
+
+    this.chartOptions.yaxis.max = maxCount + 1;
   }
 
 }
