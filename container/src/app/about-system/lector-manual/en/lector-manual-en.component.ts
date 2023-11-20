7ee8670bd5970f3f9fc3fadb78c38aa23f9fe968
Herman Zhuravsky <herman.zhuravsky@gmail.com>
2023-11-20 19:01:35 +0300

    Lector manual en (#510)
    * feat: init en structure
    
    * feat: set en manual

---
+++ container/src/app/about-system/lector-manual/en/lector-manual-en.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: './lector-manual-en.component.html',
+  styleUrls: ['../lector-manual.component.less'],
+  selector: 'app-lector-manual-en',
+})
+export class LectorManualComponentEn {}
