ee5881013f909b4cc513834758acbc76d0e6180f
Sergei Molokhvei <52610416+SergeiMol@users.noreply.github.com>
2023-11-29 23:36:45 +0300

    fixed some bugs (#514)
    * fixed some bugs
    
    * run prettier

--- modules/subjects/src/app/shared/warning-popover/warning-popover.component.ts
+++ modules/subjects/src/app/shared/warning-popover/warning-popover.component.ts
@@ -5,11 +5,11 @@ import { DialogData } from 'src/app/models/dialog-data.model'
 @Component({
   selector: 'app-warning-popover',
   templateUrl: './warning-popover.component.html',
-  styleUrls: ['./warning-popover.component.less']
+  styleUrls: ['./warning-popover.component.less'],
 })
 export class WarningPopoverComponent {
   constructor(
     public dialogRef: MatDialogRef<WarningPopoverComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) { }
+  ) {}
 }
