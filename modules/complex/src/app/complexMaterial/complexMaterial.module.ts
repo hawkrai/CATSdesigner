3ff639b6a546d3bcb2e19e234e4315eb8d5b35b6
NikitaZayatz <129108258+NikitaZayatz@users.noreply.github.com>
2024-04-15 21:34:34 +0300

    Added notification window (#584)
    * feat: added a new component to separate the materials-popover component
    
    * style: attaching a form to the left menu
    
    * style: resizing window
    
    * import notificationPopover
    
    * change text
    
    * change translation and text in editing eumk

--- modules/complex/src/app/complexMaterial/complexMaterial.module.ts
+++ modules/complex/src/app/complexMaterial/complexMaterial.module.ts
@@ -20,8 +20,10 @@ import { VarDirective } from './components/materials/add-material-popover/direct
 import * as dataRu from '../core/translate/translations_ru.json'
 import * as dataEn from '../core/translate/translations_en.json'
 import { TranslateModule, TranslatePipe } from 'educats-translate'
+import { NotificationPopoverComponent } from "./components/materials/notification-popover/notification-popover.component"
 import { HelpComponent } from "../help/help.component";
 import { PopoverModule } from 'ngx-smart-popover'
+
 
 @NgModule({
   declarations: [
@@ -37,6 +39,7 @@ import { PopoverModule } from 'ngx-smart-popover'
     LoaderComponent,
     VarDirective,
     AdaptivePopupComponent,
+    NotificationPopoverComponent,
     HelpComponent,
   ],
   imports: [
@@ -62,6 +65,7 @@ import { PopoverModule } from 'ngx-smart-popover'
     MonitoringPopoverComponent,
     AddMaterialPopoverComponent,
     AdaptivePopupComponent,
+    NotificationPopoverComponent,
   ],
 })
 export class ComplexMaterialModule {}
