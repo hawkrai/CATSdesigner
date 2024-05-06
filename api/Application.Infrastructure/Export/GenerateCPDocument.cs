494d6c49ec9f647916fd1da7439781c91f69c8d6
ArTiK5102 <artemhasansin99@gmail.com>
2024-05-06 23:27:18 +0300

    Fixed exception when downloading an empty assignment sheet

--- api/Application.Infrastructure/Export/GenerateCPDocument.cs
+++ api/Application.Infrastructure/Export/GenerateCPDocument.cs
@@ -7335,16 +7335,30 @@ namespace Application.Infrastructure.Export
 
             Run run157 = new Run();
             Text text142 = new Text() { Space = SpaceProcessingModeValues.Preserve };
-            if (string.IsNullOrWhiteSpace(awork.Student?.MiddleName))
+
+            if (string.IsNullOrWhiteSpace(awork?.Student?.MiddleName))
             {
-                text142.Text = awork is null ? "" : string.Format("{0}." +
-                                                                  " {1}", awork.Student.FirstName[0], awork.Student.LastName);
+                if (awork != null && awork.Student != null)
+                {
+                    var firstName = awork.Student.FirstName;
+                    var lastName = awork.Student.LastName;
+
+                    if (!string.IsNullOrEmpty(firstName) && !string.IsNullOrEmpty(lastName))
+                    {
+                        text142.Text = string.Format("{0}. {1}", firstName[0], lastName);
+                    }
+                    else
+                    {
+                        text142.Text = "";
+                    }
+                }
+                else
+                {
+                    text142.Text = "";
+                }
             }
-            else
-            {
-                text142.Text = string.Empty;
-            }
-            
+
+
 
             run157.Append(text142);
             ProofError proofError25 = new ProofError() { Type = ProofingErrorValues.SpellStart };
