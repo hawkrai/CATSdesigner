48d38d563916a2765bdc4a36a3020542e68b17ac
TT94ka <tt94ka@gmail.com>
2024-06-01 23:37:07 +0300

    Fixing sql query for adding a table

--- api/Db Scripts/CreateNotesTable.sql
+++ api/Db Scripts/CreateNotesTable.sql
@@ -1,9 +1,31 @@
-use LMPlatform
-create table Notes (
-    Id int primary key identity not null,
-    Text nvarchar(max),
-    Date date not null,
-    Time time not null,
-    UserId int foreign key references Users(UserId),
-    SubjectId int foreign key references Subjects(Id),
-)
+USE educatsb_LMPlatform_FITR
+GO
+CREATE TABLE [dbo].[Notes](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Text] [nvarchar](max) NULL,
+	[UserId] [int] NULL,
+	[SubjectId] [int] NULL,
+	[LecturesScheduleId] [int] NULL,
+	[LabsScheduleId] [int] NULL,
+	[PracticalScheduleId] [int] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Notes] ADD PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Notes]  WITH CHECK ADD FOREIGN KEY([LabsScheduleId])
+REFERENCES [dbo].[ScheduleProtectionLabs] ([Id])
+GO
+ALTER TABLE [dbo].[Notes]  WITH CHECK ADD FOREIGN KEY([LecturesScheduleId])
+REFERENCES [dbo].[LecturesScheduleVisitings] ([Id])
+GO
+ALTER TABLE [dbo].[Notes]  WITH CHECK ADD FOREIGN KEY([PracticalScheduleId])
+REFERENCES [dbo].[ScheduleProtectionPracticals] ([Id])
+GO
+ALTER TABLE [dbo].[Notes]  WITH CHECK ADD FOREIGN KEY([SubjectId])
+REFERENCES [dbo].[Subjects] ([Id])
+GO
+ALTER TABLE [dbo].[Notes]  WITH CHECK ADD FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([UserId])
