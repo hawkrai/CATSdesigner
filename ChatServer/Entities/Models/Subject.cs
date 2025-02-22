a646047a31eb6324ad0d1b42edfea9c975b72b95
Timofey Krupkin <facemoon2606@gmail.com>
2025-02-22 15:54:56 +0300

    feat: add subject model

---
+++ ChatServer/Entities/Models/Subject.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Models.GroupChatModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public virtual bool IsNew
+        {
+            get
+            {
+                return Id == 0;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public string Color { get; set; }
+
+        public bool IsArchive { get; set; }
+
+        public bool IsNeededCopyToBts { get; set; }
+
+        public ICollection<SubjectGroup> SubjectGroups { get; set; }
+
+        public ICollection<SubjectLecturer> SubjectLecturers { get; set; }
+    }
+}
