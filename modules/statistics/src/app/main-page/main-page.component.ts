a08cde782bdd07c5d23df4e9b894a01b77847785
mivanoffka <108406837+mivanoffka@users.noreply.github.com>
2025-04-06 20:01:51 +0300

    fix: adjust sorting so that latin-named subjects before cyrillic (#778)
    * fix: load subject info when navigated by chart-card
    
    * feat(style): add color highlighting on subject title hover
    
    * fix: adjust sorting so that latin-named subjects before cyrillic

--- modules/statistics/src/app/main-page/main-page.component.ts
+++ modules/statistics/src/app/main-page/main-page.component.ts
@@ -1,8 +1,24 @@
 import { Component, OnInit } from '@angular/core'
-
 import { StatisitcsServiceService } from '../service/statisitcs-service.service'
 import { TranslatePipe } from 'educats-translate'
 import { Message } from '../../../../../container/src/app/core/models/message'
+
+function isCyrillic(str: string): boolean {
+  return /[а-яА-ЯЁё]/.test(str)
+}
+
+function compareSubjects(a: string, b: string): number {
+  const aIsCyrillic = isCyrillic(a)
+  const bIsCyrillic = isCyrillic(b)
+
+  if (aIsCyrillic && !bIsCyrillic) {
+    return 1
+  }
+  if (!aIsCyrillic && bIsCyrillic) {
+    return -1
+  }
+  return a.localeCompare(b)
+}
 
 @Component({
   selector: 'app-main-page',
@@ -109,7 +125,7 @@ export class MainPageComponent implements OnInit {
       this.serviceService.getTeacherStatistics().subscribe((res) => {
         let i = 0
         res.SubjectStatistics.sort((a, b) =>
-          a.SubjectName.localeCompare(b.SubjectName)
+          compareSubjects(a.SubjectName, b.SubjectName)
         )
         res.SubjectStatistics.forEach((subject) => {
           this.serviceService
@@ -570,10 +586,16 @@ export class MainPageComponent implements OnInit {
   ngOnInit() {}
 
   public rerouteToSubject(subjectId: any) {
-    const message: Message = new Message()
-    message.Value = '/web/viewer/subject/' + subjectId + '#news'
-    message.Type = 'Route'
-    this.sendMessage(message)
+    const subjectMessage: Message = new Message()
+    subjectMessage.Value = subjectId
+    subjectMessage.Type = 'SubjectId'
+
+    const routeMessage: Message = new Message()
+    routeMessage.Value = '/web/viewer/subject/' + subjectId + '#news'
+    routeMessage.Type = 'Route'
+
+    this.sendMessage(subjectMessage)
+    this.sendMessage(routeMessage)
   }
 
   public sendMessage(message: Message): void {
@@ -591,7 +613,7 @@ export class MainPageComponent implements OnInit {
   }
 
   performData(subjects: any, result: any, chartList1: any, isArchive: boolean) {
-    subjects = subjects.sort((a, b) => a.Name.localeCompare(b.Name))
+    subjects = subjects.sort((a, b) => compareSubjects(a.Name, b.Name))
     this.subjectName = []
     this.series = []
     this.practMarks = []
