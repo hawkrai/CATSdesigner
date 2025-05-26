2f6452ae77f47462f7585566303735fd6062b945
Vladislav Bezmen <116635239+DOMINAtsu@users.noreply.github.com>
2025-05-26 20:41:05 +0300

    Basic algorithm (#833)
    * fix: correcting the index of the basic algorithm
    
    * fix: removed the crutch with the timer

--- modules/complex/src/app/complexMaterial/components/materials/adaptiveLearningTests/adaptive-learning-test.component.ts
+++ modules/complex/src/app/complexMaterial/components/materials/adaptiveLearningTests/adaptive-learning-test.component.ts
@@ -61,11 +61,6 @@ export class TestExecutionComponent implements OnInit {
         ),
         tap((question: TestQuestion) => {
           this.question = question
-          const threeHoursInSeconds = 3 * 60 * 60
-          this.question.Seconds = this.question.Seconds - threeHoursInSeconds
-          if (this.question.Seconds < 0) {
-            this.question.Seconds = 0
-          }
           if (!this.question.Seconds && this.question.Seconds === 0) {
             this.showTest = false
           }
