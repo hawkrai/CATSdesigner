0c136e2269515eef5e68fec36efa2cc224e69222
zhur4k <kostya.zhurovich12052@gmail.com>
2025-05-27 07:14:33 +0300

    feat: make NN test not autolocked on completed

--- api/Application.Infrastructure/KnowledgeTestsManagement/TestPassingService.cs
+++ api/Application.Infrastructure/KnowledgeTestsManagement/TestPassingService.cs
@@ -698,7 +698,9 @@ namespace Application.Infrastructure.KnowledgeTestsManagement
                     .AddFilterClause(testUnlock => testUnlock.StudentId == userId && testUnlock.TestId == testId))
                     .SingleOrDefault();
 
-                if (!GetTest(testId).ForSelfStudy && savedTestUnlock != null)
+                var test = GetTest(testId);
+
+                if (!(test.ForSelfStudy || test.ForNN) && savedTestUnlock != null)
                 {
                     repositoriesContainer.TestUnlocksRepository.Delete(savedTestUnlock);
                 }
