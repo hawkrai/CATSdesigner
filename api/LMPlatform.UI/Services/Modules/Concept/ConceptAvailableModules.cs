18b0643be4c5fa3e80aa384a23d33156c16c478f
zhur4k <kostya.zhurovich12052@gmail.com>
2025-05-28 23:34:00 +0300

    fix: fix concept available module class

--- api/LMPlatform.UI/Services/Modules/Concept/ConceptAvailableModules.cs
+++ api/LMPlatform.UI/Services/Modules/Concept/ConceptAvailableModules.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace LMPlatform.UI.Services.Modules.Concept
 {
+    [DataContract]
     public class ConceptAvailableModules
     {
+        [DataMember]
         public bool Labs { get; set; }
 
+        [DataMember]
         public bool Workshops { get; set; }
 
+        [DataMember]
         public bool Lectures { get; set; }
 
+        [DataMember]
         public bool Tests { get; set; }
 
     }
