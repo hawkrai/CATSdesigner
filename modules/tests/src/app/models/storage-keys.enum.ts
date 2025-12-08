1e3eda3fb843dc2668b5eec3e6a6eb710b873ad5
medve <medve@DESKTOP-IJSR4OU>
2025-12-08 10:13:35 -0800

    fix get file subject, delete important for selector and add enum for storage keys

---
+++ modules/tests/src/app/models/storage-keys.enum.ts
@@ -0,0 +1,21 @@
+export enum StorageKeys {
+  // SessionStorage keys
+  TestFromEUMK = 'testFromEUMK',
+  ComplexTestId = 'complexTestId',
+  EumkRoute = 'eumkRoute',
+  EumkComplexId = 'eumkComplexId',
+  ComplexId = 'complexId',
+  
+  // LocalStorage keys
+  CurrentUser = 'currentUser',
+  CurrentSubject = 'currentSubject',
+  TestsModuleTab = 'testsModule_tab',
+  Theme = 'theme',
+  SelectedComplex = 'selectedComplex',
+  Locale = 'locale',
+  Start = 'start',
+  Toggle = 'toggle',
+  DiplomProjectTab = 'diplomProject_tab',
+  CourseProjectTab = 'courseProject_tab',
+}
+
